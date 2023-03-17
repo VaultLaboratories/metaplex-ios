a799cc56cb6d49896efb0a5be89882998d326fae
Andy Boyd <andrew.boyd.ni@gmail.com>
2023-03-17 13:06:58 +0000

    fixing package resolution woes

--- Package.swift
+++ Package.swift
@@ -13,7 +13,8 @@ let package = Package(
             targets: ["Metaplex"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/VaultLaboratories/metaplex-swift-program-library", branch: "main")
+        .package(url: "https://github.com/VaultLaboratories/metaplex-swift-program-library", branch: "main"),
+        .package(url: "https://github.com/VaultLaboratories/Solana.Swift", branch: "master")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
